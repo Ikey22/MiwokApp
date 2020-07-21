c0a328d3f7d6d658d374a86c3537dc43fa263efb
Chris Barth <Christopherbartholomewokeke@gmail.com>
2020-07-21 23:02:56 +0100

    Updated FamilyActivity.kt and PhrasesActivity.kt

--- app/src/main/java/com/example/igbo/FamilyActivity.kt
+++ app/src/main/java/com/example/igbo/FamilyActivity.kt
@@ -7,7 +7,28 @@ import android.widget.ListView
 class FamilyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_family)
+        setContentView(R.layout.word_list)
+
+        val words = ArrayList<Word>()
+
+
+        words.add(Word("father", "nna"))
+        words.add(Word("mother", "nne"))
+        words.add(Word("son", "nwa nwoke"))
+        words.add(Word("daughter", "nwa nwanyi"))
+        words.add(Word("older brother", " nwanne nwoke nke okenye"))
+        words.add(Word("younger brother", "nwanne nwoke"))
+        words.add(Word("older sister", "nwanne nwanyi nke okenye"))
+        words.add(Word("younger sister", "nwanne nwanyi"))
+        words.add(Word("grandmother", "nne nne"))
+        words.add(Word("grandfather", "nna nna"))
+
+
+
+        val adapter :WordAdapter = WordAdapter(this, words)
+        val listView:ListView = findViewById(R.id.list)
+        listView.adapter=adapter
+
 
 
     }
--- app/src/main/java/com/example/igbo/PhrasesActivity.kt
+++ app/src/main/java/com/example/igbo/PhrasesActivity.kt
@@ -2,10 +2,32 @@ package com.example.igbo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class PhrasesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phrases)
+
+        val words = ArrayList<Word>()
+
+        words.add(Word("where are you going?", "ebee ka ị na-aga?"))
+        words.add(Word("what is your name?", "kedu aha gị?"))
+        words.add(Word("my name is...", "aha m bụ."))
+        words.add(Word("how are you feeling?", "kedu ka ọ dị gị?"))
+        words.add(Word("I'm feeling good", "ahụ dị m ma"))
+        words.add(Word("are you coming?", "ị ga-abịa?"))
+        words.add(Word("yes, I'm coming", "Ee, m na-abịa"))
+        words.add(Word("I'm coming", "ana m abịa"))
+        words.add(Word("let's go", "ka a pụọ"))
+        words.add(Word("come here", "bịa ebe a"))
+
+
+
+
+        val adapter :WordAdapter = WordAdapter(this, words)
+        val listView: ListView = findViewById(R.id.list)
+        listView.adapter=adapter
+
     }
 }
